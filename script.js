de1ab7afef9ea2c5d780270e27b01e68f90fad53
marenh <72942304+marenhamby@users.noreply.github.com>
2020-11-02 14:41:39 -0500

    removed code for generating each element. decided to change how things are pulled and just going to make arrays with the different character options

--- script.js
+++ script.js
@@ -32,25 +32,6 @@ var includeNumbers = confirm("Would you like your password to include numbers?")
 var includeSymbols = confirm("Would you like your password to include numbers?")
 
 
-// **List of functions used to generate a random value for each of the 
-// types of criteria (lowercase, uppercase, number, symbol).
-
-function getLower () {
-  return String.fromCharCode(Math.floor(Math.random()*26) + 97);
-}
-
-function getUpper () {
-  return String.fromCharCode(Math.floor(Math.random()*26) + 65);
-}
-
-function getNumber () {
-  return String.fromCharCode(Math.floor(Math.random()*10) + 48);
-}
-
-function getSymbol () {
-  var symbolList=[" ", "!", "U+0022", "#", "$", "%", "&", "U+0027", "(", ")", "*", "+", ",", "-", ".", "/", ":", ";", "<", "=", ">", "?", "@", "[", "U+005C", "]", "^", "_", "`", "{", "|", "}", "~",]
-  return symbolList[Math.floor(Math.random()*symbolList.length)];
-}
 
 
 // Add event listener to generate button
