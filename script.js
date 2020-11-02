448ee961eea9aaefa326f6af67788ba05c896b8c
marenh <72942304+marenhamby@users.noreply.github.com>
2020-11-01 21:42:16 -0500

    added the functions that output numbers from the four different criteria for the password.

--- script.js
+++ script.js
@@ -10,5 +10,26 @@ function writePassword() {
 
 }
 
+// **List of functions used to generate a random value for each of the 
+// types of criteria (lowercase, uppercase, number, symbol).
+
+function getLower () {
+  return String.fromCharCode(Math.floor(Math.random()*26) + 97);
+}
+
+function getUpper () {
+  return String.fromCharCode(Math.floor(Math.random()*26) + 65);
+}
+
+function getNumber () {
+  return String.fromCharCode(Math.floor(Math.random()*10) + 48);
+}
+
+function getSymbol () {
+  var symbolList=[" ", "!", "U+0022", "#", "$", "%", "&", "U+0027", "(", ")", "*", "+", ",", "-", ".", "/", ":", ";", "<", "=", ">", "?", "@", "[", "U+005C", "]", "^", "_", "`", "{", "|", "}", "~",]
+  return symbolList[Math.floor(Math.random()*symbolList.length)];
+}
+
+
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
